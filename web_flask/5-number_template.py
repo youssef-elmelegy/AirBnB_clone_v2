730b8cdf6cbf5eba8cefe46ad7553b8489f9a655
youssef-elmelegy <youssefelmelegy999@gmail.com>
2024-07-27 09:40:09 -0700

    fix file 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -8,7 +8,6 @@ Routes:
     /number/<n>: Displays 'n is a number' only if <n> is an integer.
     /number_template/<n>: Displays an HTML page only if <n> is an integer.
 """
-
 from flask import Flask
 from flask import render_template
 
@@ -17,33 +16,32 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """Displays 'Hello HBNB!'."""
+    """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Displays 'HBNB'."""
+    """Displays 'HBNB'"""
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
-    """
-    Displays 'C' followed by the value of <text>.
+    """Displays 'C' followed by the value of <text>
     """
     text = text.replace("_", " ")
-    return f"C {text}"
+    return "C {}".format(text)
 
 
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python(text="is cool"):
-    """
-    Displays 'Python' followed by the value of <text>.
+    """Displays 'Python' followed by the value of <text>
     """
     text = text.replace("_", " ")
     return "Python {}".format(text)
+
 
 @app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
