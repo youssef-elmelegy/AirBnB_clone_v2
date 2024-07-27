343ebbbe88c241559e0f0a4fd6982d9c5d04e0ab
youssef-elmelegy <youssefelmelegy999@gmail.com>
2024-07-27 07:56:10 -0700

    numbers and ints

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Routes:
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+    /c/<text>: Displays 'C' followed by the value of <text>.
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!'."""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays 'HBNB'."""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """ display “C ” followed by the value of the text variable"""
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+@app.route("/python/<text>", strict_slashes=False)
+@app.route("/python", strict_slashes=False)
+def python(text="is cool"):
+    """
+    display “Python ”, followed by the value of the text variable
+    The default value of text is “is cool”"""
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
