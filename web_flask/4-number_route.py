37a3d4ff536b04eb61e4edb4b2f41315eb5ec486
youssef-elmelegy <youssefelmelegy999@gmail.com>
2024-07-27 09:27:16 -0700

    fix 4

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 """Starts a Flask web application.
 
-The application listens on 0.0.0.0, port 5000
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+    /c/<text>: Displays 'C' followed by the value of <text>.
+    /python/(<text>): Displays 'Python' followed by the value of <text>.
+    /number/<n>: Displays 'n is a number' only if <n> is an integer.
 """
 from flask import Flask
+from flask import abort
 
 app = Flask(__name__)
 
@@ -22,26 +29,30 @@ def hbnb():
 
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
-    """
-    Displays 'C' followed by the value of <text>.
+    """Displays 'C' followed by the value of <text>.
+
+    Replaces any underscores in <text> with slashes.
     """
     text = text.replace("_", " ")
-    return f"C {text}"
+    return "C {}".format(text)
 
 
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python(text="is cool"):
-    """
-    Displays 'Python' followed by the value of <text>.
+    """Displays 'Python' followed by the value of <text>.
+
+    Replaces any underscores in <text> with slashes.
     """
     text = text.replace("_", " ")
     return "Python {}".format(text)
+
 
 @app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
     """Displays 'n is a number' only if n is an integer."""
     return "{} is a number".format(n)
 
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0")
