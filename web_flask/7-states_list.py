bdf9da52965f805ba3a077fc96f237a1bd1e81bb
youssef-elmelegy <youssefelmelegy999@gmail.com>
2024-07-28 00:03:24 -0700

    add

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Starts a Flask web application.
+
+The application listens on 0.0.0.0, port 5000.
 Routes:
     /states_list: HTML page with a list of all State objects in DBStorage.
 """
@@ -13,6 +15,8 @@ app = Flask(__name__)
 @app.route("/states_list", strict_slashes=False)
 def states_list():
     """Displays an HTML page with a list of all State objects in DBStorage.
+
+    States are sorted by name.
     """
     states = storage.all("State")
     return render_template("7-states_list.html", states=states)
@@ -25,4 +29,4 @@ def teardown(exc):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0")
