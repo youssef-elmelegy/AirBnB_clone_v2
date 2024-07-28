6526acde7f847e518ea180c3bfb4b9bc4adc8091
youssef-elmelegy <youssefelmelegy999@gmail.com>
2024-07-27 23:08:08 -0700

    add

--- models/state.py
+++ models/state.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+import models
+from models.base_model import BaseModel, Base
+from models.city import City
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="delete")
 
     if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
